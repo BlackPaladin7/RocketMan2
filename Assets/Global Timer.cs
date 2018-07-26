ca8c01bc6b258b4e3723e714d2c49557a5828735
SMCHCN\coltonevans <richardthelionhearted@gmail.com>
2018-07-26 09:14:25 -0500

    Level Timer

---
+++ Assets/Global Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GlobalTimer : MonoBehaviour
+{
+    public Text TimerText;
+    public float startTime;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        startTime = Time.time;
+        float t = Time.time - startTime;
+
+        string minutes = ((int) t / 60).ToString();
+        string seconds = (t % 60).ToString("f1");
+
+        TimerText.text = minutes + ":" + seconds;
+	}
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public Text TimerText;
+    private float startTime;
+
+	// Use this for initialization
+	void Start ()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float t = Time.time - startTime;
+
+        string minutes = ((int) t / 60).ToString();
+        string seconds = (t % 60).ToString("f1");
+
+        TimerText.text = minutes + ":" + seconds;
+	}
+}
